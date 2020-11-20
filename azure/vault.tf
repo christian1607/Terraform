4ca00b86a78f2130add37da67804301a1541e7f1
Christian Altamirano <christian.altamirano.ayala@gmail.com>
2020-11-19 23:48:21 -0500

    Azure - Fix Se agrego KeyVault

--- azure/vault.tf
+++ azure/vault.tf
@@ -41,10 +41,10 @@ module "keyvault_policy" {
   kv_vault_id                = module.keyvault.id
   kv_tenant_id               = data.azurerm_client_config.current.tenant_id
   kv_object_id               = data.azurerm_client_config.current.object_id
-  kv_key_permissions         = ["backup", "create", "delete", "deleteissuers", "get", "getissuers", "import", "list", "listissuers", "managecontacts", "manageissuers", "purge", "recover", "restore", "setissuers", "update"]
-  kv_secret_permissions      = ["backup", "create", "decrypt", "delete", "encrypt", "get", "import", "list", "purge", "recover", "restore", "sign", "unwrapKey", "update", "verify", "wrapKey"]
-  kv_certificate_permissions = ["backup", "delete", "get", "list", "purge", "recover", "restore", "set"]
-  kv_storage_permissions     = ["backup", "delete", "deletesas", "get", "getsas", "list", "listsas", "purge", "recover", "regeneratekey", "restore", "set", "setsas", "update"]
+  kv_key_permissions         = ["backup", "create", "delete", "deleteissuers", "get", "getissuers", "import", "list", "listissuers", "managecontacts", "manageissuers", "purge", "recover", "restore", "setissuers", "update",]
+  kv_secret_permissions      = ["backup", "create", "decrypt", "delete", "encrypt", "get", "import", "list", "purge", "recover", "restore", "sign", "unwrapKey", "update", "verify", "wrapKey",]
+  kv_certificate_permissions = ["backup", "delete", "get", "list", "purge", "recover", "restore", "set",]
+  kv_storage_permissions     = ["backup", "delete", "deletesas", "get", "getsas", "list", "listsas", "purge", "recover", "regeneratekey", "restore", "set", "setsas", "update",]
 
 }
 
