e65aaf560e9b3585a9b88c0f88eedec5e8a3d459
Christian Altamirano <christian.altamirano.ayala@gmail.com>
2020-11-26 16:25:02 -0500

    Azure - CosmosDB module

--- azure/main.tf
+++ azure/main.tf
@@ -11,10 +11,7 @@ terraform {
 
 
 provider "azurerm" {
-  subscription_id = "72a70c6f-90c3-4aca-a78d-6e538352c901"
-  client_id       = "d2660c40-0d2c-48ee-b478-4af3831f1b93"
-  client_secret   = "X~CLsM8mVvCWf4ktucJ5Zz2RXN_J5GZcQ2"
-  tenant_id       = "a6c36135-a633-4acd-94b2-06b54f179f92"
+  
   features {}
 
 }
